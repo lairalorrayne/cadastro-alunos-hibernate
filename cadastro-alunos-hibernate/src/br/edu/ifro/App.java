0e7d92d14b9f5ddec870202cb3542b65ccb0b2e4
04202603230 <04202603230@JIPA-LABIN05-05.ifro.local>
2018-04-05 14:49:20 -0400

    Cadastro-alunos-hibernate

---
+++ cadastro-alunos-hibernate/src/br/edu/ifro/App.java
@@ -0,0 +1,50 @@
+package br.edu.ifro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class App extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Apenas para exemplo
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Fulano");
+        
+        em.getTransaction().begin();
+        em.persist(aluno1);
+        em.getTransaction().commit();
+        
+        
+        //Fim exemplo
+        
+        
+        
+        
+        
+        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
